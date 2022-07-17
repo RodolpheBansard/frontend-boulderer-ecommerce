c8e33b586055fdfdeab3bda3c9fc24a05ddf2b37
Rodolphe <rodolphebansard@yahoo.com>
2022-07-17 16:43:11 +0200

    product List by category

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Product} from "../../models/product";
 import {Observable} from "rxjs";
 import {ProductService} from "../../services/product.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
@@ -10,10 +11,26 @@ import {ProductService} from "../../services/product.service";
 })
 export class ProductListComponent implements OnInit {
 
-  products$ : Observable<Product[]> = new Observable<Product[]>();
+  products : Product[] = [];
 
-  constructor(private productService: ProductService) {
-    this.products$ = this.productService.getProductList();
+  categoryId = 2;
+
+  constructor(private productService: ProductService,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe(() => {
+      const hasCategoryId = this.activatedRoute.snapshot.paramMap.has('categoryId');
+      if(hasCategoryId){
+        // @ts-ignore
+        this.categoryId = +this.activatedRoute.snapshot.paramMap.get('categoryId');
+      }
+
+      this.productService.getProductListByProductCategory(this.categoryId).subscribe((products) => {
+        this.products = products;
+      })
+    });
+
+
+
   }
 
   ngOnInit(): void {
